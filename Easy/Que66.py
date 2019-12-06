7b1c711a5477aebf6b5ea4213f5a77b4643a7268
HuangZengPei <zphuang96@163.com>
2019-12-06 09:53:01 +0800

    ""

--- Easy/Que66.py
+++ Easy/Que66.py
@@ -4,12 +4,16 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
+
         n = len(digits)
         while n >= 1:
             plus = digits[n-1] + 1
             digits[n-1] = plus % 10
             if plus >= 10:
                 n -= 1
+                if n == 0:  # 考虑数字要增加一位的情况
+                    l1 = [1]
+                    digits = l1 + digits
             else:
                 break
         return digits
