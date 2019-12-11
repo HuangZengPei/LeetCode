1581829bb3a777c5f6618d316c8f5a1afad3213d
HuangZengPei <zphuang96@163.com>
2019-12-11 17:09:04 +0800

    rotate matrix

--- Middle/Que48.py
+++ Middle/Que48.py
@@ -6,9 +6,11 @@ class Solution(object):
         原地旋转图像
         """
         n = len(matrix[0])
+        # 首先转置矩阵
         for i in range(n):
             for j in range(i,n):
                 matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
 
+        # 之后将每一行再反转
         for i in range(n):
             matrix[i].reverse()
