6c32a836392b617c378e12548a7d50f30ecb273e
HuangZengPei <zphuang96@163.com>
2019-12-14 22:11:49 +0800

    判断两棵树是否相同

---
+++ Easy/Que100.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    
+    # 不能单独用前序遍历，[1,2]以及[1,null,2]结果相同
+    def preOrder(self,p,list):
+            if p == None:
+                return 
+            else:
+                list.append(p.val)
+                preOrder(p.left)
+                preOrder(p.right)
+                
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """        
+        list1 = []
+        list2 = []
+        self.preOrder(p,list1)
+        self.preOrder(q,list2)
+        return list1 == list2
+        
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """        
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        else:
+            return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
