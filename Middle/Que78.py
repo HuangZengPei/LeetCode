111f0e5d6d5de99149202bd883d45ffca3b5541f
HuangZengPei <zphuang96@163.com>
2020-01-04 17:42:41 +0800

    求子集，动态规划及回溯

--- Middle/Que78.py
+++ Middle/Que78.py
@@ -1,22 +1,35 @@
 class Solution(object):
     # 求子集，动态规划
+    # def subsets(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: List[List[int]]
+    #     """
+    #     dp = [[]]
+    #     for i in range(len(nums)):
+    #         dp = dp + [each+[nums[i]] for each in dp]
+    # 
+    #     return dp
+        
     def subsets(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        dp = [[]]
-        for i in range(len(nums)):
-            dp = dp + [each+[nums[i]] for each in dp]
-            
-        return dp
+        result = []
+        solution = []
+        self.backtracking(0,nums,solution,result)
+        return result
         
     # 回溯法
-    def backtracking(self,index,nums,k,solution,res):
-        if len(solution) == k:
-            res.append(solution[:])
-            return
-        for i in range(index,n+1):
-            solution.append(i)
-            self.backtracking(i+1,n,k,solution,res)
+    def backtracking(self,index,nums,solution,res):
+        res.append(solution[:])
+        for i in range(index,len(nums)):
+            solution.append(nums[i])
+            self.backtracking(i+1,nums,solution,res)
+            print("solution" + solution)
             solution.pop()
+            
+test = Solution()
+nums = [1,2,3]
+test.subsets(nums)
