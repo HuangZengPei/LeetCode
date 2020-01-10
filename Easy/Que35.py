77be0c75e9c801b449504ab29a7dbda236015ba6
HuangZengPei <zphuang96@163.com>
2020-01-10 20:57:41 +0800

    二分搜索插入的位置

--- Easy/Que35.py
+++ Easy/Que35.py
@@ -1,19 +1,40 @@
 class Solution(object):
+    # def searchInsert(self, nums, target):
+    #     """
+    #     :type nums: List[int]
+    #     :type target: int
+    #     :rtype: int
+    #     """
+    #     if not nums:return 0
+    #     for i in range(len(nums)):
+    #         if target > nums[i]:
+    #             continue
+    #         else:
+    #             return i
+    #     if i == len(nums) - 1:
+    #         return i+1
+            
     def searchInsert(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
+        二分查找
         """
         if not nums:return 0
-        for i in range(len(nums)):
-            if target > nums[i]:
-                continue
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            middle = int(low + (high - low) / 2)
+            if nums[middle] == target:
+                return middle
+            if nums[middle] < target:
+                low = middle + 1
             else:
-                return i
-        if i == len(nums) - 1:
-            return i+1
+                high = middle - 1
+        return low
+
 
 test = Solution()
-nums = [1]
-print(test.searchInsert(nums,0))
+nums = [1,3,5,6]
+print(test.searchInsert(nums,7))
