f3dca683054eabeac2824c1572f2d73cbbf32140
HuangZengPei <zphuang96@163.com>
2020-02-25 13:54:41 +0800

    动态规划判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。

---
+++ Middle/Que139.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        s_len = len(s)
+        dp = [False] * (s_len+1)
+        dp[0] = True
+        for i in range(s_len):
+            for j in range(i+1, n+1):
+                if dp[i] and s[i:j] in wordDict:
+                    dp[j] = True
+        return dp[-1]
