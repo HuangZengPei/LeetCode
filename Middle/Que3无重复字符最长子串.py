ca24921ec84084e667c1376480f4d0a2d2cc033c
HuangZengPei <zphuang96@163.com>
2020-05-02 23:25:23 +0800

    滑动窗口求最长子串

--- Middle/Que3无重复字符最长子串.py
+++ Middle/Que3无重复字符最长子串.py
@@ -16,6 +16,25 @@ class Solution:
             if cur_len > max_len:max_len = cur_len
             lookup.add(s[i])
         return max_len
+        
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s:return 0
+        left = 0
+        lookup = []
+        n = len(s)
+        max_len = 0
+        cur_len = 0
+        for i in range(n):
+            if s[i] in lookup:
+                index = lookup.index(s[i])
+                lookup.append(s[i])
+                lookup = lookup[index+1:]
+                cur_len = len(lookup)
+            else:
+                lookup.append(s[i])
+                cur_len += 1
+            if cur_len > max_len:max_len = cur_len
+        return max_len
 
 test_unit = Solution()
 test = ['p', 'w', 'w', 'k', 'e','w']
